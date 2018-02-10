0384f7a7b9df9196e137fa515a754b861b8e5df6
richardroma <34776990+richardroma@users.noreply.github.com>
2018-02-10 14:45:01 -0500

    Assignment #1
    Read in depression dataset from text file and analyze the income data.  First, I added column names to the dataset and then plotted the income data versus the employment status.  After the initial scatter diagram, I normalized the income data and displayed normal probability plots and histograms to analyze the distribution.

---
+++ Roma_BIA652_HW1.R
@@ -0,0 +1,56 @@
+#Richard Roma
+#BIA 652
+#Homework 1
+
+#Problem 3.3
+
+#Read Depress.Txt data into R with read.table().
+
+depres <- read.table("C:/Users/Dell User/Documents/Stevens MBA/Spring 18/BIA 652/Data_for_HW/Depress.txt", header = FALSE)
+
+#Check the data read is accurate.
+head(depres)
+
+#Assign Column Names for the Depress Dataset based on codebook for easier analysis.
+colnames(depres) <- c('ID','Sex','Age','Marital','Educat','Employ','Income','Relig','C1','C2','C3','C4','C5','C6',
+                           'C7','C8','C9','C10','C11','C12','C13','C14','C15','C16','C17','C18','C19','C20','CESD','Cases',
+                           'Drink','Health','Regdoc','Treat','Beddays','Acuteill','Chronill')
+
+#Check the headers were added correctly.
+head(depres)
+
+#Plot the income versus employment status from the dataset.
+plot(depres$Employ, depres$Income,xlab='Employment Status',ylab='Income')
+
+
+#Problem 4.1
+#Create a variable equal to the negative of one divided by the cubic root of income.
+depres$Income_cbrt <- (-1)/(depres$Income^(1/3))
+
+#Check the new variable Income_cbrt
+head(depres$Income)
+head(depres$Income_cbrt)
+
+#Display a normal probability plot of the variable Income_cbrt
+qqnorm(depres$Income_cbrt)
+qqline(depres$Income_cbrt)
+
+
+#Problem 4.2
+#Take the logarithm to base 10 of the income variable in the depression dataset.
+depres$Income_log <- log10(depres_data$Income)
+
+#Histogram of Income Histogram of Log(Income)
+hist(depres$Income,main="Histogram of Income",xlab="Income",col="yellow")
+hist(depres$Income_log,main="Histogram of Log(Income)",xlab="Income",col="yellow")
+
+#Normal Probability Plots of Income and log(Income)
+qqnorm(depres$Income)
+qqline(depres$Income)
+
+qqnorm(depres$Income_log)
+qqline(depres$Income_log)
+
+
+#Problem 4.3
+
